5eb2fb941bb7634ac521146b37a79d2ff1775be2
Danielle Martin <danielle.jaclyn.martin@gmail.com>
2021-08-25 15:45:48 -0700

    prep twitter api call for flask app

---
+++ tweet.py
@@ -0,0 +1,35 @@
+import json
+import requests
+from config import token
+
+keyword1 = '(America OR USA OR United States of America)'
+keyword2 =  ' -is:retweet -is:reply lang:en'
+max_results = 10
+
+search_url = "https://api.twitter.com/2/tweets/search/recent"
+
+# US used as an example query as it is a required field
+query_params = {'query': keyword1+keyword2,
+                'max_results': max_results,
+               }
+
+
+def bearer_oauth(r):
+    r.headers["Authorization"] = f"Bearer {token}"
+    return r
+
+def connect_to_endpoint(url, params):
+    response = requests.get(url, auth=bearer_oauth, params=params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+def api_call():
+    tweets = []
+    json_response = connect_to_endpoint(search_url, query_params)
+    for response in json_response['data']:
+        tweets.append( {'tweet': response['text'], 'sentiment score': ''}) 
+    return tweets
+
+
