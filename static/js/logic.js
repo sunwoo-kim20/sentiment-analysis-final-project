98986604db892ffbd23370b6999b462b2165995d
Jordan Roessle <screamaimfire@me.com>
2021-08-25 16:40:57 -0700

    small changes to webpage stuff, made a basic model in firstAttemp

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,18 @@
+var counter = 0;
+
 function clickedFace(choice) {
-    
+    updateTweet();
+    updatePrediction(choice)
 }
+
+function updateTweet() {
+    counter += 1;
+    d3.select(".tweet").text(counter);
+}
+
+function updatePrediction(choice) {
+    d3.select("#modelPredict").text(`Our model predicts this tweet has a ${choice} sentiment.`)
+}
+
+updatePrediction("");
+updateTweet("");
