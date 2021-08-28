e23b09679c8fa151ddf5d6acece4b5d9cd3fd22c
Sunwoo Kim <s.kim32415@gmail.com>
2021-08-28 11:33:28 -0700

    added sqlalchemy and class constructors

--- app.py
+++ app.py
@@ -10,12 +10,32 @@ from bson import json_util
 from modelPredict import predictModel
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, BigInteger, Date
+from sqlalchemy.orm import Session
+from datetime import datetime
 
 
 # initialize flask
 app = Flask(__name__)
+
+
+# Initialize global variable
 tweet_data= {}
+
+# Create database connection String
+rds_connection_string = "postgres:password@localhost:5432/sentiment_db"
+
+
+Base = declarative_base()
+# Create class for updating data
+class tweet(Base):
+    __tablename__ = 'tweet'
+    id = Column(BigInteger, primary_key=True)
+    tweet = Column(String())
+    sentiments = Column(Integer)
+    predicted_sentiments = Column(Integer)
+	time_data_inserted = Column(Date)
+
 
 @app.route("/")
 def home():
@@ -30,8 +50,18 @@ def apicalled():
 
 @app.route("/positive_update")
 def positive_update():
-	tweet_data['time_data_inserted'] =
+	tweet_data['time_data_inserted'] = datetime.now()
 	tweet_data['predicted_sentiments'] =
+
+	# Create connection to SQL database
+	engine = create_engine(f'postgresql://{rds_connection_string}')
+	conn = engine.connect()
+	session = Session(bind = engine)
+
+
+	# Create tweet instance
+
+
 
 @app.route("/negative_update")
 def negative_update():
