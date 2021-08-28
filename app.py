af94ac667f3117d26d72db0e2bb982bcbf5c4992
Sunwoo Kim <s.kim32415@gmail.com>
2021-08-28 11:17:56 -0700

    added dependencies and starter code for updating sql

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# Dependencies
+# ----------------------------------
 from flask import Flask, render_template, redirect
 import requests
 import tweet
@@ -6,9 +8,14 @@ import os
 import json
 from bson import json_util
 from modelPredict import predictModel
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float
+
 
 # initialize flask
 app = Flask(__name__)
+tweet_data= {}
 
 @app.route("/")
 def home():
@@ -20,5 +27,16 @@ def apicalled():
 	tweet_data['sentiment'] = predictModel(tweet_data['tweet'])
 	return json.loads(json_util.dumps(tweet_data))
 
+
+@app.route("/positive_update")
+def positive_update():
+	tweet_data['time_data_inserted'] =
+	tweet_data['predicted_sentiments'] =
+
+@app.route("/negative_update")
+def negative_update():
+	tweet_data['time_data_inserted'] =
+	tweet_data['predicted_sentiments'] =
+
 if __name__ == "__main__":
-	app.run(debug=True)	
+	app.run(debug=True)
