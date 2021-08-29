01a51f4a01b93f5b1338802b93a242c49ef7a8b0
Danielle Martin <danielle.jaclyn.martin@gmail.com>
2021-08-29 11:33:44 -0700

    added db creation/insertion to api call

--- tweet.py
+++ tweet.py
@@ -1,6 +1,24 @@
 import json
 import requests
 from config import token
+from sqlalchemy import Table, Column, Integer, String, MetaData, Date, create_engine, insert, Float
+from sqlalchemy.ext.declarative import declarative_base
+
+rds_connection_string = "postgres:postgres@localhost:5432/sentiment_db"
+engine = create_engine(f'postgresql://{rds_connection_string}')
+conn = engine.connect()
+Base = declarative_base()
+Base.metadata.create_all(conn)
+meta = MetaData()
+tweet_data = Table(
+   'tweet_data', meta, 
+   Column('id', String, primary_key = True), 
+   Column('tweet', String), 
+   Column('sentiments', Integer),
+   Column('predicted_sentiments', Float),
+   Column('time_data_inserted', Date) 
+)
+meta.create_all(engine)
 
 keyword1 = '(America OR USA OR United States of America)'
 keyword2 =  ' -is:retweet -is:reply lang:en'
@@ -29,7 +47,15 @@ def api_call():
     tweets = []
     json_response = connect_to_endpoint(search_url, query_params)
     for response in json_response['data']:
-        tweets.append( {'id': response['id'], 'tweet': response['text'], 'sentiment': ''}) 
+        tweets.append( {'id': response['id'], 'tweet': response['text'], 'sentiment': ''})
+        conn = engine.connect()
+
+        with conn:
+            conn.execute(insert(tweet_data),[{"id":response['id'],
+                "tweet":response['text'],
+                "sentiments":9,
+                "predicted_sentiments":9,
+                "time_data_inserted":'1/1/01'}]) 
     return tweets
 
-
+api_call()
