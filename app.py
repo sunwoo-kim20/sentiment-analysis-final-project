88c93a7c7326128b82a6e46c2d94abbd93f8c07e
Danielle Martin <danielle.jaclyn.martin@gmail.com>
2021-08-29 14:15:25 -0700

    added load_tweet to pull from db

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 # Dependencies
 # ----------------------------------
-from flask import Flask, render_template, redirect, jsonify
-import requests
-import tweet
+from flask import Flask, render_template, redirect, jsonify, request
 from flask import send_from_directory
-import os
-import json
-from modelPredict import predictModel
+import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Float, BigInteger, Date
+from sqlalchemy import MetaData, update, Table
 from sqlalchemy.orm import Session
 from datetime import datetime
 from data import return_data
+from modelPredict import predictModel
+# import tweet
 
 
 # initialize flask
@@ -20,20 +18,25 @@ app = Flask(__name__)
 
 
 # Initialize global variable
-tweet_data= {}
+tweet_dict= {}
 
 # Create database connection String
 rds_connection_string = "postgres:postgres@localhost:5432/sentiment_db"
+engine = create_engine(f'postgresql://{rds_connection_string}')
 
-Base = declarative_base()
-# Create class for updating data
-class Tweet(Base):
-	__tablename__ = 'tweet'
-	id = Column(BigInteger, primary_key=True)
-	tweet = Column(String())
-	sentiments = Column(Integer)
-	predicted_sentiments = Column(Integer)
-	time_data_inserted = Column(Date)
+metadata = MetaData(engine)
+tweet_data = Table('tweet_data', metadata, autoload=True, autoload_with=engine)
+
+
+# Base = declarative_base()
+# # Create class for updating data
+# class Tweet(Base):
+# 	__tablename__ = 'tweet_data'
+# 	id = Column(String, primary_key=True)
+# 	tweet = Column(String())
+# 	sentiments = Column(Integer)
+# 	predicted_sentiments = Column(Integer)
+# 	time_data_inserted = Column(Date)
 
 @app.route("/")
 def home():
@@ -47,55 +50,92 @@ def voting():
 def statistics():
 	return render_template("statistics.html")
 
-@app.route("/apicall")
-def apicalled():
-	tweet_data = tweet.api_call()[0]
-	tweet_data['predicted_sentiments'] = predictModel(tweet_data['tweet'])
-	return jsonify(tweet_data)
+# @app.route("/apicall")
+# def apicalled():
+# 	tweet.api_call()
 
-@app.route("/positive_update")
+@app.route("/load_tweet")
+def load_tweet():
+	conn = engine.connect()
+	df = pd.read_sql_query('select * from tweet_data WHERE tweet_data.sentiments = 9', con=conn)
+	df = df.iloc[0]
+	tweet_dict = {
+		"id":df['id'],
+		"tweet":df['tweet'],
+		"sentiments":int(df['sentiments']),
+		"predicted_sentiments":df["predicted_sentiments"],
+		"time_data_inserted":df['time_data_inserted']
+	}
+
+	tweet_dict['predicted_sentiments'] = predictModel(tweet_dict['tweet'])
+	print(tweet_dict)
+
+	update_db = (
+		update(tweet_data).
+		where(tweet_data.c.id == tweet_dict['id']).
+		values(predicted_sentiments=tweet_dict['predicted_sentiments'])
+	)
+	conn.execute(update_db)
+
+	return jsonify(tweet_dict)
+
+@app.route("/positive_update", methods=['POST'])
 def positive_update():
-	tweet_data['time_data_inserted'] = datetime.now()
-	tweet_data['sentiment'] = 1
+	tweetID = request.form['tweetid']
+	tweet_dict['id'] = tweetID
+	tweet_dict['time_data_inserted'] = datetime.now()
+	tweet_dict['sentiment'] = 1
+	print(tweet_dict)
 
 	# Create connection to SQL database
-	engine = create_engine(f'postgresql://{rds_connection_string}')
 	conn = engine.connect()
-	session = Session(bind = engine)
 
-	# Create tweet instance
-	tweet_upload = Tweet(
-		id = tweet_data.id,
-		tweet = tweet_data.tweet,
-		sentiments = tweet_data.sentiment,
-		predicted_sentiments = tweet_data.predicted_sentiments,
-		time_data_inserted = tweet_data.time_data_inserted)
+	# Create object update
+	tweet_update = (
+		update(tweet_data).
+		where(tweet_data.c.id == tweet_dict.id).
+		values(sentiments=tweet_dict.sentiments, time_data_inserted=tweet_dict.time_data_inserted)
+	)
+	conn.execute(tweet_update)
 
-	# Add new instance to database
-	session.add(tweet_upload)
-	session.commit()
-
-@app.route("/negative_update")
+@app.route("/negative_update", methods=['POST'])
 def negative_update():
-	tweet_data['time_data_inserted'] = datetime.now()
-	tweet_data['sentiment'] = 0
+	tweetID = request.form['tweetid']
+	tweet_dict['id'] = tweetID
+	tweet_dict['time_data_inserted'] = datetime.now()
+	tweet_dict['sentiment'] = 0
+	print(tweet_dict)
 
 	# Create connection to SQL database
-	engine = create_engine(f'postgresql://{rds_connection_string}')
 	conn = engine.connect()
-	session = Session(bind = engine)
 
-	# Create tweet instance
-	tweet_upload = Tweet(
-		id = tweet_data.id,
-		tweet = tweet_data.tweet,
-		sentiments = tweet_data.sentiment,
-		predicted_sentiments = tweet_data.predicted_sentiments,
-		time_data_inserted = tweet_data.time_data_inserted)
+	# Create object update
+	tweet_update = (
+		update(tweet_data).
+		where(tweet_data.c.id == tweet_dict.id).
+		values(sentiments=tweet_dict.sentiments, time_data_inserted=tweet_dict.time_data_inserted)
+	)
+	conn.execute(tweet_update)
 
-	# Add new instance to database
-	session.add(tweet_upload)
-	session.commit()
+	# tweet_data['time_data_inserted'] = datetime.now()
+	# tweet_data['sentiment'] = 0
+
+	# # Create connection to SQL database
+	# engine = create_engine(f'postgresql://{rds_connection_string}')
+	# conn = engine.connect()
+	# session = Session(bind = engine)
+
+	# # Create tweet instance
+	# tweet_upload = Tweet(
+	# 	id = tweet_data.id,
+	# 	tweet = tweet_data.tweet,
+	# 	sentiments = tweet_data.sentiment,
+	# 	predicted_sentiments = tweet_data.predicted_sentiments,
+	# 	time_data_inserted = tweet_data.time_data_inserted)
+
+	# # Add new instance to database
+	# session.add(tweet_upload)
+	# session.commit()
 
 @app.route("/data")
 def datacalled():
