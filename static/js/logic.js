006c21e6dc058104ba50bcf20235ee3f57c468e6
Rob G <popestarkiller@icloud.com>
2021-09-08 15:45:51 -0700

    fixed the voting bug

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,36 +4,26 @@ function clickedFace(choice) {
     // call route to give vote to flask 
     if (choice === 'Positive') {
         $.post("/positive_update", data);
-        location.reload();
     }
     if (choice === 'Negative') {
         $.post( "/negative_update", data);
-        location.reload();
     }
     if (choice === 'Neutral') {
         $.post("/neutral_update", data);
-        location.reload();
+
 
     }
 
-    // update tweet
-    // updateTweet();
-    // location.reload();
+    updateTweet();
 }
 
-// function noSentiment(choice) {
-//     var data = d3.select(".tweetholder").data()[0]
-//     choice == 'Neutral'
-//     updateTweet();
-// }
+
 
 function updateTweet() {
     d3.json("/load_tweet").then(data =>{
         // give data to the tweet and change the tweet text
 
         d3.select(".tweetholder").data([data]).text(data.tweet)
-        console.log(data)
-        console.log("update tweet")
         // make prediction 
         // var prediction;
         // if (data.predicted_sentiments_rd >= .5) {
